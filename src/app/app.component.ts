88f5a9169f63a1c96e105e98ca0ea4b142ddb9b6
MateuszKita <mkita.zg@gmail.com>
2018-11-16 08:02:46 +0100

    UZ-8 hide menu if navigated to /login url

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { SecurityService } from './security/security.service';
 import { take } from 'rxjs/operators';
-import { Router, NavigationEnd } from '@angular/router';
+import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
+
+const LOGIN_URL = '/login';
 
 @Component({
   selector: 'app-root',
@@ -10,22 +12,29 @@ import { Router, NavigationEnd } from '@angular/router';
 })
 export class AppComponent implements OnInit {
   public tokenIsValid = false;
+  public showMenu: boolean;
   private token: string;
 
   constructor(
     private readonly securityService: SecurityService,
-    private readonly router: Router
+    private readonly router: Router,
+    private readonly route: ActivatedRoute
   ) {}
 
   ngOnInit(): void {
+    this.checkTokenValidityOnNavigate();
+  }
+
+  // TO DO: remove nested subscription
+  private checkTokenValidityOnNavigate(): void {
     this.token = this.securityService.getToken();
     this.router.events.subscribe(event => {
       if (event instanceof NavigationEnd) {
+        this.changeShowMenuValue(event.url);
         this.securityService
           .chechTokenIsValid(this.token)
           .pipe(take(1))
           .subscribe(isValid => {
-            console.log(isValid);
             this.tokenIsValid = isValid;
             if (!this.tokenIsValid) {
               this.router.navigate(['login']);
@@ -33,5 +42,9 @@ export class AppComponent implements OnInit {
           });
       }
     });
+  }
+
+  private changeShowMenuValue(route: string): void {
+    this.showMenu = route !== LOGIN_URL;
   }
 }
