c46a0da1fadcc941ca1b89ceb01703257957db9a
MateuszKita <mkita.zg@gmail.com>
2019-01-06 20:23:43 +0100

    UZ-11 create basic view for teams

--- src/app/views/teams/teams.component.ts
+++ src/app/views/teams/teams.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Team } from 'src/app/models/teams.model';
 
 @Component({
   selector: 'app-teams',
@@ -6,7 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./teams.component.scss']
 })
 export class TeamsComponent implements OnInit {
+  public displayedColumns: string[] = ['id', 'name'];
+  public dataSource: Team[] = [];
+
   constructor() {}
 
   ngOnInit() {}
+
+  public createTeam(): void {
+    console.log('create team');
+  }
 }
--- src/app/views/teams/teams.module.ts
+++ src/app/views/teams/teams.module.ts
@@ -3,9 +3,22 @@ import { CommonModule } from '@angular/common';
 
 import { TeamsRoutingModule } from './teams-routing.module';
 import { TeamsComponent } from './teams.component';
+import {
+  MatExpansionModule,
+  MatTableModule,
+  MatIconModule,
+  MatButtonModule
+} from '@angular/material';
 
 @NgModule({
-  imports: [CommonModule, TeamsRoutingModule],
+  imports: [
+    CommonModule,
+    TeamsRoutingModule,
+    MatExpansionModule,
+    MatTableModule,
+    MatIconModule,
+    MatButtonModule
+  ],
   declarations: [TeamsComponent]
 })
 export class TeamsModule {}
